e1b5c698196e988b7252bf801c02458f3db28a2d
Haimmichaeli90 <haimmichaeli95@gmail.com>
2024-07-26 16:42:44 +0300

    add API to show map, and some style and gender options

--- js/controller.js
+++ js/controller.js
@@ -16,11 +16,24 @@ function onSubmit(event) {
     userData.email = document.getElementById('email').value
     userData.Age = document.getElementById('age').value
     userData.birthDate = document.getElementById('dob').value
+    userData.gender = document.getElementById('gender').value
     userData.txtColor = document.getElementById('txtColor').value
     userData.bgColor = document.getElementById('bgColor').value
+    userData.initialZoom = document.getElementById('initialZoom').value
+    userData.initialLocationLat = document.getElementById('initialLocationLat').value
+    userData.initialLocationLng = document.getElementById('initialLocationLng').value
 
     save(userData)
     
     document.body.style.backgroundColor = userData.bgColor
     document.body.style.color = userData.txtColor
+    
+    
 }
+
+
+// function validateAgeWithBirthDate(age,dob){
+//     const birthYear = new Date(dob).getFullYear()
+//     const currentYear = new Date().getFullYear()
+//     return (currentYear - birthYear) == age
+// }
