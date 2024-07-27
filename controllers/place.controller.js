d338a9b0b50505ed63ffd7213732186e74aa2e59
Haimmichaeli90 <haimmichaeli95@gmail.com>
2024-07-27 17:43:36 +0300

    adding current location to map

--- controllers/place.controller.js
+++ controllers/place.controller.js
@@ -14,6 +14,12 @@ function onInit() {
         zoom: 14
     })
     renderPlaces()
+    createPlaceByUser()
+    addYourLocationButton(gMap)
+    
+}
+
+function createPlaceByUser(){
     gMap.addListener('click', ev => {
         const name = prompt('Place name?', 'Place 1')
         const lat = ev.latLng.lat()
@@ -21,8 +27,6 @@ function onInit() {
         addPlace(name, lat, lng, gMap.getZoom())
         renderPlaces()
     })
-    
-    
 }
 
 function renderPlaces() {
@@ -36,9 +40,11 @@ function renderPlaces() {
         placeItem.innerHTML = `
             <span>${place.name}</span>
             <button onclick="onPanToPlace('${place.id}')">Show on Map</button>
-        `;
+            <button onclick="onRemovePlace('${place.id}')">Delete</button>
+        `
+        
         placesList.appendChild(placeItem)
-    });
+    })
 }
 
 function getPlaceById(placeId) {
@@ -76,4 +82,59 @@ function closePlacesList() {
     const dialog = document.getElementById('places-dialog')
     dialog.style.display = 'none'
     checkbox.checked = false
+}
+
+function addYourLocationButton(map) {
+    const controlDiv = document.createElement('div')
+    const firstChild = document.createElement('button')
+    firstChild.style.backgroundColor = '#fff'
+    firstChild.style.border = 'none'
+    firstChild.style.outline = 'none'
+    firstChild.style.width = '28px'
+    firstChild.style.height = '28px'
+    firstChild.style.borderRadius = '2px'
+    firstChild.style.boxShadow = '0 1px 4px rgba(0,0,0,0.3)'
+    firstChild.style.cursor = 'pointer'
+    firstChild.style.marginRight = '10px'
+    firstChild.style.marginLeft = '10px'
+    firstChild.style.padding = '0'
+    firstChild.title = 'Your Location'
+    controlDiv.appendChild(firstChild)
+
+    const secondChild = document.createElement('div')
+    secondChild.style.margin = '5px'
+    secondChild.style.width = '18px'
+    secondChild.style.height = '18px'
+    secondChild.style.backgroundImage = 'url(https://maps.gstatic.com/tactile/mylocation/mylocation-sprite-2x.png)'
+    secondChild.style.backgroundSize = '180px 18px'
+    secondChild.style.backgroundPosition = '0 0'
+    secondChild.style.backgroundRepeat = 'no-repeat'
+    firstChild.appendChild(secondChild)
+
+    google.maps.event.addListener(map, 'center_changed', () => {
+        secondChild.style['background-position'] = '0 0'
+    })
+
+    firstChild.addEventListener('click', () => {
+        let imgX = '0'
+        const animationInterval = setInterval(() => {
+            imgX = imgX === '-18' ? '0' : '-18'
+            secondChild.style['background-position'] = `${imgX}px 0`
+        }, 500)
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(position => {
+                const latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
+                map.setCenter(latlng)
+                clearInterval(animationInterval)
+                secondChild.style['background-position'] = '-144px 0'
+            })
+        } else {
+            clearInterval(animationInterval)
+            secondChild.style['background-position'] = '0 0'
+        }
+    })
+
+    controlDiv.index = 1
+    map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(controlDiv)
 }
